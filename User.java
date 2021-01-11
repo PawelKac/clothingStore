ea737a932458c2db54671216397301669bd73f5e
Paweł <pawel.j.kaczmarczyk01@gmail.com>
2021-01-11 14:32:05 +0100

    Create User.java

---
+++ User.java
@@ -0,0 +1,13 @@
+
+public class User {
+
+    Long id;
+    String login;
+    String password;
+
+    public User(Long id, String login, String password){
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+}

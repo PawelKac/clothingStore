47c208fced6f1b9a7eb5385b908589e0524532be
Paweł <pawel.j.kaczmarczyk01@gmail.com>
2021-01-13 11:20:57 +0100

    Update User.java

--- User.java
+++ User.java
@@ -1,13 +1,25 @@
 
 public class User {
 
-    Long id;
-    String login;
-    String password;
+    private Long id;
+    private String login;
+    private String password;
 
     public User(Long id, String login, String password){
         this.id = id;
         this.login = login;
         this.password = password;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public String getPassword(){
+        return password;
     }
 }

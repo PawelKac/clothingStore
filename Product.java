7c0f4230ceee587a798961670b715ea8d736f729
Paweł <pawel.j.kaczmarczyk01@gmail.com>
2021-01-13 11:32:41 +0100

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,12 +1,12 @@
 
 public class Product {
 
-    Long id;
-    String productName;
-    Float price;
-    Float weight;
-    String color;
-    Integer productCount;
+    private Long id;
+    private String productName;
+    private Float price;
+    private Float weight;
+    private String color;
+    private Integer productCount;
 
     public Product(Long id, String productName, Float price, Float weight, String color, Integer productCount){
         this.id = id;
@@ -15,5 +15,37 @@ public class Product {
         this.weight = weight;
         this.color = color;
         this.productCount = productCount;
+    }
+
+    public void setPrice(Float price){ //ceny chcemy zmieniać
+        this.price = price;
+    }
+
+    public void setProductCount(Integer productCount){ //tak samo liczbę produktów
+        this.productCount = productCount;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getProductName(){
+        return productName;
+    }
+
+    public Float getPrice(){
+        return price;
+    }
+
+    public Float getWeight(){
+        return  weight;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public Integer getProductCount(){
+        return productCount;
     }
 }

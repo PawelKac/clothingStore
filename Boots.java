b49ef9134cf578e9fc7da6173d3ae45ed4f33448
Paweł <pawel.j.kaczmarczyk01@gmail.com>
2021-01-14 14:56:05 +0100

    Update Boots.java

--- Boots.java
+++ Boots.java
@@ -1,12 +1,20 @@
 
 public class Boots extends Product {
 
-    String size;
-    Boolean isNaturalSkin;
-    
+    private String size;
+    private Boolean isNaturalSkin;
+
     public Boots(Long id, String productName, Float price, Float weight, String color, Integer productCount, String size, Boolean isNaturalSkin){
         super(id, productName,price,weight, color,productCount);
         this.size = size;
         this.isNaturalSkin = isNaturalSkin;
+    }
+
+    public Integer getSize(){
+        return size;
+    }
+
+    public Boolean getIsNaturalSkin(){
+        return isNaturalSkin;
     }
 }

937d86d593d8b4814507230ecb12f3060e468f25
Paweł <pawel.j.kaczmarczyk01@gmail.com>
2021-01-15 15:32:46 +0100

    Update User.java

--- User.java
+++ User.java
@@ -22,4 +22,14 @@ public class User {
     public String getPassword(){
         return password;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
+
